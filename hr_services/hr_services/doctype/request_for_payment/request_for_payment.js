20b3fd1708323f7cf97083b637c9bb988c7cd8ef
UsamaNaveed9 <usamanaveed9263@gmail.com>
2023-12-19 13:59:40 +0000

    fect:Interdepartmental linked is done. Request for payment from hr to finance, on submission jv and sales invoice created

--- hr_services/hr_services/doctype/request_for_payment/request_for_payment.js
+++ hr_services/hr_services/doctype/request_for_payment/request_for_payment.js
@@ -2,7 +2,73 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Request For Payment', {
-	// refresh: function(frm) {
+	setup(frm) {
+		frm.fields_dict['items'].grid.get_field("item").get_query = function(doc, cdt, cdn) {
+			return {
+				filters: [
+					['Item', 'item_group', 'in',frm.doc.expense_type],
+				]
+			}
+        }
 
-	// }
+		frm.fields_dict['items'].grid.get_field("employee_no").get_query = function(doc, cdt, cdn) {
+			return {
+				filters: [
+					['Employee', 'project', 'in',frm.doc.project],
+				]
+			}
+        }
+		
+	}
 });
+
+frappe.ui.form.on("Request for Payment Details", {
+	item:function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		if(row.item){
+			frappe.db.get_value("Item Price", {"item_code":row.item, "selling":1}, "price_list_rate").then((r)=>{
+				if(r.message.price_list_rate){
+					let price = 0;
+					price = r.message.price_list_rate;
+
+					row.rate = price;
+					if(!row.qty){
+						row.qty = 1;
+					}
+					row.amount = row.qty * price;
+					let total = 0;
+					for(let i in frm.doc.items){
+						total += frm.doc.items[i].amount;
+					}
+					frm.set_value("total_amount", total);
+					frm.refresh();
+				}
+			})
+		}
+	},
+	qty:function(frm, cdt, cdn){
+		calculate_amount(frm, cdt, cdn);
+	},
+	rate:function(frm, cdt, cdn){
+		calculate_amount(frm, cdt, cdn);
+	},
+	items_remove(frm,cdt,cdn){
+		let total = 0;
+		for(let i in frm.doc.items){
+				total += frm.doc.items[i].amount;
+			}
+		frm.set_value("total_amount", total);
+		frm.refresh();
+	}
+});
+
+function calculate_amount(frm, cdt, cdn){
+	let row = locals[cdt][cdn];
+	row.amount = row.qty * row.rate;
+	let total = 0;
+	for(let i in frm.doc.items){
+		total += frm.doc.items[i].amount;
+	}
+	frm.set_value("total_amount", total);
+	frm.refresh();
+}
--- hr_services/hr_services/doctype/request_for_payment/request_for_payment.py
+++ hr_services/hr_services/doctype/request_for_payment/request_for_payment.py
@@ -1,8 +1,51 @@
 # Copyright (c) 2023, Elite Resources and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class RequestForPayment(Document):
-	pass
+	def on_submit(self):
+		#creating journal entry on the approval of request of payment
+		new_doc = frappe.new_doc("Journal Entry")
+		new_doc.voucher_type = "Journal Entry"
+		new_doc.posting_date = self.date
+		new_doc.company = self.company
+		for it in self.items:
+			row = new_doc.append("accounts",{})
+			row.account = frappe.db.get_value("Item",{"name":it.item},"account_for_jv")
+			row.debit_in_account_currency = it.amount
+
+		row = new_doc.append("accounts",{})
+		row.account = frappe.db.get_value("Company",{"name":self.company},"default_bank_account")
+		row.credit_in_account_currency = self.total_amount
+				
+		new_doc.save()
+		new_doc.submit()
+
+		#creating sales invoice on the approval of request of payment
+		si = frappe.new_doc("Sales Invoice")
+		si.customer = frappe.db.get_value("Project",{"name":self.project},"customer")
+		si.set_posting_time = 1
+		si.posting_date = self.date
+		si.due_date = self.date
+		si.issue_date = self.date
+		si.project = self.project
+			
+		for it in self.items:
+			si_item = frappe.new_doc("Sales Invoice Item")
+			si_item.item_code = it.item
+			si_item.employee_id = it.employee_no
+			si_item.employee_name = it.employee_name
+			si_item.qty = it.qty
+			si_item.rate = it.rate	
+			si.append("items", si_item)
+
+		si_tax = frappe.new_doc("Sales Taxes and Charges")
+		si_tax.charge_type = "On Net Total"
+		si_tax.account_head = "VAT 15% - ERC"
+		si_tax.description = "VAT 15%"
+		si_tax.rate = 15
+		si.append("taxes", si_tax)
+
+		si.save(ignore_permissions=True)
