bec1a2e1d7ed5b0ed2b4af5e05ad334de1868182
UsamaNaveed9 <usamanaveed9263@gmail.com>
2024-01-17 14:59:00 +0000

    fect:Accrued End of Service frontend is developed

---
+++ hr_services/hr_services/doctype/accrued_end_of_services/accrued_end_of_services.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Elite Resources and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Accrued End of Services', {
+	// refresh: function(frm) {
+
+	// }
+});
