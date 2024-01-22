050e1a24bae62e5a9aedc620f652ff2295779a1c
UsamaNaveed9 <usamanaveed9263@gmail.com>
2024-01-22 12:15:14 +0000

    fect:skip creating sales invoice from the RFP if project elite hq

--- hr_services/hr_services/doctype/request_for_payment/request_for_payment.py
+++ hr_services/hr_services/doctype/request_for_payment/request_for_payment.py
@@ -26,34 +26,35 @@ class RequestForPayment(Document):
 			new_doc.save()
 			new_doc.submit()
 
-			#creating sales invoice on the approval of request of payment
-			si = frappe.new_doc("Sales Invoice")
-			si.customer = frappe.db.get_value("Project",{"name":self.project},"customer")
-			si.set_posting_time = 1
-			si.posting_date = self.date
-			si.due_date = self.date
-			si.issue_date = self.date
-			si.project = self.project
-				
-			for it in self.items:
-				si_item = frappe.new_doc("Sales Invoice Item")
-				si_item.item_code = it.item
-				si_item.employee_id = it.employee_no
-				si_item.employee_name = it.employee_name
-				si_item.qty = it.qty
-				si_item.rate = it.rate	
-				si.append("items", si_item)
+			#creating sales invoice on the approval of request of payment and skip for project PROJ-0018 (Elite HQ)
+			if self.project != "PROJ-0018":
+				si = frappe.new_doc("Sales Invoice")
+				si.customer = frappe.db.get_value("Project",{"name":self.project},"customer")
+				si.set_posting_time = 1
+				si.posting_date = self.date
+				si.due_date = self.date
+				si.issue_date = self.date
+				si.project = self.project
+					
+				for it in self.items:
+					si_item = frappe.new_doc("Sales Invoice Item")
+					si_item.item_code = it.item
+					si_item.employee_id = it.employee_no
+					si_item.employee_name = it.employee_name
+					si_item.qty = it.qty
+					si_item.rate = it.rate	
+					si.append("items", si_item)
 
-			si_tax = frappe.new_doc("Sales Taxes and Charges")
-			si_tax.charge_type = "On Net Total"
-			si_tax.account_head = "VAT 15% - ERC"
-			si_tax.description = "VAT 15%"
-			si_tax.rate = 15
-			si.append("taxes", si_tax)
+				si_tax = frappe.new_doc("Sales Taxes and Charges")
+				si_tax.charge_type = "On Net Total"
+				si_tax.account_head = "VAT 15% - ERC"
+				si_tax.description = "VAT 15%"
+				si_tax.rate = 15
+				si.append("taxes", si_tax)
 
-			si.custom_request_for_payment = self.name
-			si.remarks = f"{self.expense_type} from Request for Payment"
-			si.save(ignore_permissions=True)
+				si.custom_request_for_payment = self.name
+				si.remarks = f"{self.expense_type} from Request for Payment"
+				si.save(ignore_permissions=True)
 
 		elif self.expense_type == "Supplier Payment":
 			#creating Payment entry on the approval of request of payment
