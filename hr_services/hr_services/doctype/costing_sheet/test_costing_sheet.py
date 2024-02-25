6892724d8b5c60bc7c8b908ad1dcd09fa52f0183
UsamaNaveed9 <usamanaveed9263@gmail.com>
2024-02-25 14:01:56 +0000

    feat:Frontend of Costing sheet is developed.

---
+++ hr_services/hr_services/doctype/costing_sheet/test_costing_sheet.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Elite Resources and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCostingSheet(FrappeTestCase):
+	pass
