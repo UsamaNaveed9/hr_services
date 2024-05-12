c97c56edb29d733ba4c1adbc9eb0ea27137b8a5b
UsamaNaveed9 <usamanaveed9263@gmail.com>
2024-05-12 14:20:37 +0000

    feat: Qiwa & Gosi customiztion added in the job offer

--- hr_services/customjs/job_offer.js
+++ hr_services/customjs/job_offer.js
@@ -1,12 +1,55 @@
 // Copyright (c) 2023, Elite Resources and contributors
 // For license information, please see license.txt
 
+frappe.provide("erpnext.job_offer");
+
 frappe.ui.form.on('Job Offer', {
 	before_save(frm){
 		if(frm.doc.custom_salary_details){
 			calculate_salary(frm);
 		}
 	},
+	refresh: function (frm) {
+		if ((!frm.doc.__islocal) && (frm.doc.status == 'Accepted') && (frm.doc.custom_qiwa_status == 'Accepted by Candidate' || frm.doc.custom_qiwa_status == 'Accepted by His/Her Sponsor') 
+			&& (frm.doc.custom_gosi_status == 'Accepted') && (frm.doc.docstatus === 1) && (!frm.doc.__onload || !frm.doc.__onload.employee)) {
+			frm.add_custom_button(__('Create Employee'),
+				function () {
+					erpnext.job_offer.make_employee(frm);
+				}
+			);
+		}
+		if ((!frm.doc.__islocal) && (frm.doc.status == 'Accepted') && (frm.doc.custom_job_title_on_visa)
+			&& (frm.doc.docstatus === 1) && (!frm.doc.__onload || !frm.doc.__onload.employee)) {
+			frm.add_custom_button(__('Create Contract'),
+				function () {
+					erpnext.job_offer.make_contract(frm);
+				}
+			);
+		}
+
+	},
+	custom_nationality: function(frm) {
+		if(frm.doc.custom_nationality == 'Saudi'){
+			frm.set_value("custom_country", "Saudi Arabia");
+		}
+		else{
+			frm.set_value("custom_country", "");
+		}
+	},
+	custom_applicant_type: function(frm){
+		if(frm.doc.custom_applicant_type == 'Local'){
+			frm.set_value("custom_job_title_on_visa", "");
+		}
+		else if(frm.doc.custom_applicant_type == 'Overseas'){
+			frm.set_value("custom_qiwa_status", "");
+			frm.set_value("custom_gosi_status", "");
+		}
+		else{
+			frm.set_value("custom_qiwa_status", "");
+			frm.set_value("custom_gosi_status", "");
+			frm.set_value("custom_job_title_on_visa", "");
+		}
+	}
 });
 
 frappe.ui.form.on("Contract Details", {
@@ -26,3 +69,11 @@ function calculate_salary(frm){
 	frm.set_value("custom_total_salary", total);
 	frm.refresh();
 }
+
+erpnext.job_offer.make_employee = function (frm) {
+	console.log("YES")
+	frappe.model.open_mapped_doc({
+		method: "hr_services.custompy.job_offer.make_employee",
+		frm: frm
+	});
+};
---
+++ hr_services/custompy/job_offer.py
@@ -0,0 +1,42 @@
+#Copyright (c) 2023, Elite Resources and contributors
+#For license information, please see license.txt
+
+import frappe
+from frappe.model.mapper import get_mapped_doc
+
+@frappe.whitelist()
+def make_employee(source_name, target_doc=None):
+	def set_missing_values(source, target):
+		target.personal_email, target.first_name = frappe.db.get_value(
+			"Job Applicant", source.job_applicant, ["email_id", "applicant_name"]
+		)
+		source_doc = frappe.get_doc("Job Offer",source_name)
+		others = 0
+		for sd in source_doc.custom_salary_details:
+			if sd.salary_components == "Basic Salary":
+				target.basic_salary = sd.amount
+			elif sd.salary_components == "Housing Allowance":
+				target.housing_allowance = sd.amount
+			elif sd.salary_components == "Transportations Allowance":
+				target.transport_allowance = sd.amount
+			else:
+				others += sd.amount
+		target.food_allowance = others
+		target.ctc = source_doc.custom_total_salary
+	doc = get_mapped_doc(
+		"Job Offer",
+		source_name,
+		{
+			"Job Offer": {
+				"doctype": "Employee",
+				"field_map": {"applicant_name": "employee_name", 
+							"offer_date": "scheduled_confirmation_date",
+							"custom_project": "project",
+							"custom_salutation": "salutation",
+							"custom_country": "nationality"},
+			}
+		},
+		target_doc,
+		set_missing_values,
+	)
+	return doc
