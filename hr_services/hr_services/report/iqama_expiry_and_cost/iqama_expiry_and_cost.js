6d0b1835ff71f885a665e9b6435377b79921e79b
UsamaNaveed9 <usamanaveed9263@gmail.com>
2024-05-28 13:56:22 +0000

    feat:Iqama Expiry and Cost

---
+++ hr_services/hr_services/report/iqama_expiry_and_cost/iqama_expiry_and_cost.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2024, Elite Resources and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Iqama Expiry and Cost"] = {
+	"filters": [
+		{
+			"fieldname":"month",
+			"label": __("Month"),
+			"fieldtype": "Select",
+			"options": "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec",
+			"default": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov",
+				"Dec"][frappe.datetime.str_to_obj(frappe.datetime.get_today()).getMonth()],
+		},
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company")
+		}
+	]
+};
---
+++ hr_services/hr_services/report/iqama_expiry_and_cost/iqama_expiry_and_cost.py
@@ -0,0 +1,84 @@
+# Copyright (c) 2024, Elite Resources and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+
+
+def execute(filters=None):
+	if not filters:
+		filters = {}
+
+	columns = get_columns()
+	data = get_employees(filters)
+
+	return columns, data
+
+
+def get_columns():
+	columns = [
+			{"label": _("Project ID"), "fieldname": "project_id", "fieldtype": "Link","options":"Project", "width": 100},
+			{"label": _("Project Name"), "fieldname": "project_name", "fieldtype": "Data", "width": 120},
+			{"label": _("Employee ID"), "fieldname": "employee_id", "fieldtype": "Link","options":"Employee", "width": 120},
+			{"label": _("Employee Name"), "fieldname": "employee_name", "fieldtype": "Data", "width": 200},
+			{"label": _("Iqama No"), "fieldname": "iqama_no", "fieldtype": "Data", "width": 110},
+			{"label": _("Iqama Expiry Date"), "fieldname": "iqama_expiry_date", "fieldtype": "Date", "width": 120},
+			{"label": _("Hijri Iqama Expiry Date"), "fieldname": "custom_iqama_expiry_date_in_hijri", "fieldtype": "Data", "width": 120},
+			{"label": _("Iqama Duration"), "fieldname": "iqama_duration", "fieldtype": "Data", "width": 120},
+			{"label": _("Iqama Cost"), "fieldname": "iqama_cost", "fieldtype": "Currency", "width": 110},
+			{"label": _("WL Duration"), "fieldname": "wl_duration", "fieldtype": "Data", "width": 110},
+			{"label": _("WL Cost"), "fieldname": "wl_cost", "fieldtype": "Currency", "width": 120}
+		]
+	
+	return columns
+
+
+def get_employees(filters):
+	conditions = get_conditions(filters)
+	data = frappe.db.sql(
+		"""select project as project_id, project_name,name as employee_id, employee_name, iqama_national_id as iqama_no, iqama_expiry_date, 
+			custom_iqama_expiry_date_in_hijri, company
+		  	from tabEmployee where status = 'Active' %s"""
+		% conditions,
+		as_dict=1,
+	)
+
+	for rec in data:
+		if rec.project_id:
+			project_doc = frappe.get_doc("Project",rec.project_id)
+			iqama_duration = project_doc.custom_iqama_duration
+			wl_duration = project_doc.custom_working_license
+			fees_doc = frappe.get_doc("Iqama and WL Fees", rec.company)
+			iqama_cost = (fees_doc.iqama_fee / 12) * int(iqama_duration)
+			wl_cost = (fees_doc.working_license_fee / 12) * int(wl_duration)
+
+			rec["iqama_duration"] = str(iqama_duration) + ' Months'
+			rec["iqama_cost"] = iqama_cost
+			rec["wl_duration"] = str(wl_duration) + ' Months'
+			rec["wl_cost"] = wl_cost
+
+	return data
+
+def get_conditions(filters):
+	conditions = ""
+	if filters.get("month"):
+		month = [
+			"Jan",
+			"Feb",
+			"Mar",
+			"Apr",
+			"May",
+			"Jun",
+			"Jul",
+			"Aug",
+			"Sep",
+			"Oct",
+			"Nov",
+			"Dec",
+		].index(filters["month"]) + 1
+		conditions += " and month(iqama_expiry_date) = '%s'" % month
+
+	if filters.get("company"):
+		conditions += " and company = '%s'" % filters["company"].replace("'", "\\'")
+
+	return conditions
