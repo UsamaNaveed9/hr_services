4132fbfee63f47596e86138a8f788b37f284b5e5
UsamaNaveed9 <usamanaveed9263@gmail.com>
2024-09-17 13:25:44 +0000

    fix:Duplication validation condition changed

--- hr_services/custom_core_addition/job_requisition.py
+++ hr_services/custom_core_addition/job_requisition.py
@@ -1,6 +1,35 @@
 import frappe
 from frappe.model.mapper import get_mapped_doc
+from frappe.utils import format_duration, get_link_to_form, time_diff_in_seconds
+from frappe import _
 
+#expected_compensation and description condition added into the validate_duplicates funciton
+#also passed the salary in frappe throw.
+
+def validate_duplicates(self):
+		duplicate = frappe.db.exists(
+			"Job Requisition",
+			{
+				"designation": self.designation,
+				"department": self.department,
+				"requested_by": self.requested_by,
+				"status": ("not in", ["Cancelled", "Filled"]),
+				"name": ("!=", self.name),
+				"expected_compensation": self.expected_compensation,
+				"description": self.description,
+			},
+		)
+
+		if duplicate:
+			frappe.throw(
+				_("A Job Requisition for {0} requested by {1} with same salary {2} and description already exists: {3}").format(
+					frappe.bold(self.designation),
+					frappe.bold(self.requested_by),
+					frappe.bold(self.expected_compensation),
+					get_link_to_form("Job Requisition", duplicate),
+				),
+				title=_("Duplicate Job Requisition"),
+			)
 
 #Project and Project Name added in core file function
 #Function Name: make_job_opening(source_name, target_doc=None)
